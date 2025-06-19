35281cec56e699e9576ad046b6daef9d765fd9af
ambilisudhakaran <ambilisudhakaran14@outlook.com>
2025-06-19 22:31:42 +0530

    Updated files

--- 6. Dictionaries and Sets/contact_list.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter name: ")
-phone = input("Enter phone: ")
-contacts[name] = phone
-print("Contact saved!")
-
-# Show the contact
-print("\nSaved Contacts:")
-for name in contacts:
-    print(name, ":", contacts[name])

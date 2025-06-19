d39eb4bf7616e81f92c8ccbc13e2bbcc20f33a41
ambilisudhakaran <ambilisudhakaran14@outlook.com>
2025-06-19 22:24:59 +0530

    Using elif for multiple conditions

---
+++ 8. Using elif for multiple conditions/grade_evaluation.py
@@ -0,0 +1,10 @@
+score = int(input("Enter Your Score: "))
+
+if score >= 90:
+    print("Your Grade is: A")
+elif score >= 80:
+    print("Your Grade is: B")
+elif score >= 70:
+    print("Your Grade is: C")
+else:
+    print("Your Grade is: F")
---
+++ 8. Using elif for multiple conditions/multiple_outcomes.py
@@ -0,0 +1,13 @@
+subject = input("Enter Your Subject: ")
+
+if subject == "math":
+    print("You chose Math")
+
+elif subject == "science":
+    print("You chose Science")
+
+elif subject == "english":
+    print("You chose English")
+
+else:
+    print("Good choice!")

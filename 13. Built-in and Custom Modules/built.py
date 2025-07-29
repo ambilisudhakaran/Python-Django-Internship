9e383e9b5ef3b4c7306bf5a77d443699477d2962
ambilisudhakaran <ambilisudhakaran14@outlook.com>
2025-07-29 20:03:48 +0530

    Built-in and Custom Modules

---
+++ 13. Built-in and Custom Modules/built.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+number = random.randint(1, 10)
+print("Random Number is:", number)
+
+double = number * 2
+print("Double of the number:", double)
---
+++ 13. Built-in and Custom Modules/math_random.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+number = random.randint(1, 10)
+print("Random Number is:", number)
+
+square_root = math.sqrt(number)
+print(f"Square root of {number}: {square_root}")

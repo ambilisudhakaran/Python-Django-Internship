1fa6d75318db43e58a3aa1f25ac6d7fb6bbfc9e0
ambilisudhakaran <ambilisudhakaran14@outlook.com>
2025-07-29 20:03:19 +0530

    Defining and Using Functions

---
+++ 12. Defining and Using Functions/break_code.py
@@ -0,0 +1,6 @@
+def get_name():
+    name = input("What is your name? ")
+    return name
+
+user_name = get_name()
+print("Welcome,", user_name)
---
+++ 12. Defining and Using Functions/function.py
@@ -0,0 +1,5 @@
+def my_function():
+    print("Hello from the function")
+
+my_function()
+
